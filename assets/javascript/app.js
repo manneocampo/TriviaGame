3f1b35cafa7745df67212779c086717fed16cb13
Michelle Ocampo <manneocampo@gmail.com>
2017-09-24 18:03:03 -0400

    Added a different basic setup & started js

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+var triviaGame = {
+			questionOne: ,
+			questionTwo: ,
+			questionThree: ,
+			questionFour: ,
+			questionFive: ,
+			questionSix: ,
+			questionSeven: ,
+			questionEight: ,
+			questionNine: ,
+			questionTen: , 
+			correctAnswers: 0,
+			incorrectAnswers: 0,
+			unanswered: 0, 
+		reset: function() {
+			this.correctAnswers=0;
+			this.incorrectAnswers=0;
+			this.unanswered=0;
+		},
+		gamePlay: function() {
+
+		}
+}
